fcfb43151eaeefa9742e25576381462a76cf2322
logicp <logicp@logicp.ca>
2023-09-29 21:05:08 -0400

    minimal app

---
+++ src/app.cpp
@@ -0,0 +1,121 @@
+// Copyright (c) 2013 The Chromium Embedded Framework Authors. All rights
+// reserved. Use of this source code is governed by a BSD-style license that
+// can be found in the LICENSE file.
+
+#include "app.hpp"
+
+#include <string>
+
+#include "include/cef_browser.h"
+#include "include/cef_command_line.h"
+#include "include/views/cef_browser_view.h"
+#include "include/views/cef_window.h"
+#include "include/wrapper/cef_helpers.h"
+#include "handler.hpp"
+
+namespace {
+
+// When using the Views framework this object provides the delegate
+// implementation for the CefWindow that hosts the Views-based browser.
+class SimpleWindowDelegate : public CefWindowDelegate {
+ public:
+  explicit SimpleWindowDelegate(CefRefPtr<CefBrowserView> browser_view)
+      : browser_view_(browser_view) {}
+
+  void OnWindowCreated(CefRefPtr<CefWindow> window) override {
+    // Add the browser view and show the window.
+    window->AddChildView(browser_view_);
+    window->Show();
+
+    // Give keyboard focus to the browser view.
+    browser_view_->RequestFocus();
+  }
+
+  void OnWindowDestroyed(CefRefPtr<CefWindow> window) override {
+    browser_view_ = nullptr;
+  }
+
+  bool CanClose(CefRefPtr<CefWindow> window) override {
+    // Allow the window to close if the browser says it's OK.
+    CefRefPtr<CefBrowser> browser = browser_view_->GetBrowser();
+    if (browser) {
+      return browser->GetHost()->TryCloseBrowser();
+    }
+    return true;
+  }
+
+  CefSize GetPreferredSize(CefRefPtr<CefView> view) override {
+    return CefSize(800, 600);
+  }
+
+ private:
+  CefRefPtr<CefBrowserView> browser_view_;
+
+  IMPLEMENT_REFCOUNTING(SimpleWindowDelegate);
+  DISALLOW_COPY_AND_ASSIGN(SimpleWindowDelegate);
+};
+
+class SimpleBrowserViewDelegate : public CefBrowserViewDelegate
+{
+ public:
+  SimpleBrowserViewDelegate() {}
+
+  bool OnPopupBrowserViewCreated(CefRefPtr<CefBrowserView> browser_view,
+                                 CefRefPtr<CefBrowserView> popup_browser_view,
+                                 bool is_devtools) override
+  {
+    CefWindow::CreateTopLevelWindow(new SimpleWindowDelegate(popup_browser_view));
+    return true;
+  }
+
+ private:
+  IMPLEMENT_REFCOUNTING(SimpleBrowserViewDelegate);
+  DISALLOW_COPY_AND_ASSIGN(SimpleBrowserViewDelegate);
+};
+
+}  // namespace
+
+SimpleApp::SimpleApp() {}
+
+void SimpleApp::OnContextInitialized()
+{
+  CEF_REQUIRE_UI_THREAD();
+
+  CefRefPtr<CefCommandLine> command_line =
+      CefCommandLine::GetGlobalCommandLine();
+
+  const bool use_views = command_line->HasSwitch("use-views");
+  LOG(INFO) << "Using views: " << use_views;
+
+  CefRefPtr<SimpleHandler> handler(new SimpleHandler(use_views));
+  CefBrowserSettings       browser_settings;
+
+  std::string url = command_line->GetSwitchValue("url");
+  if (url.empty())
+    url = "http://www.google.com";
+
+  if (use_views)
+  {
+    // Create the BrowserView.
+    CefRefPtr<CefBrowserView> browser_view = CefBrowserView::CreateBrowserView(
+        handler, url, browser_settings, nullptr, nullptr,
+        new SimpleBrowserViewDelegate());
+
+    // Create the Window. It will show itself after creation.
+    CefWindow::CreateTopLevelWindow(new SimpleWindowDelegate(browser_view));
+  }
+  else
+  {
+    // Information used when creating the native window.
+    CefWindowInfo window_info;
+
+    // Create the first browser window.
+    CefBrowserHost::CreateBrowser(window_info, handler, url, browser_settings,
+                                  nullptr, nullptr);
+  }
+}
+
+CefRefPtr<CefClient> SimpleApp::GetDefaultClient()
+{ // Called when a new browser window is created via the Chrome runtime UI.
+  return SimpleHandler::GetInstance();
+}
